27e0d8e447065f0b6aad2e0dbff5d836f4ac1a83
DominikKraut <dominik.kraut@applifting.cz>
2023-02-09 13:11:18 +0100

    Lol

--- opakovani.js
+++ opakovani.js
@@ -10,7 +10,7 @@
 // null;
 // (Boolean = true), false;
 
-//Object literal
+//Object literal, prototype chaining, každý objekt má maximálně jeden prototyp, poslední prototyp, respektive konec chainu má hodnotu NULL
 
 const animal = {
   dna: 123,
@@ -29,6 +29,7 @@ const dog = {
   },
 };
 
+// 
 Object.setPrototypeOf(dog, animal);
 console.log(dog.dna);
 
@@ -74,3 +75,20 @@ console.log(newObj); // { foo: 1, bar: 2, baz: 3 }
 const arr1 = [1, 2, 3];
 const arr2 = [4, 5, 6];
 const arr3 = [...arr1, ...arr2];
+
+// Optional chaining - pokud volám properties objektů tak to často hodíl error protože objekt může být undefined, to nechceme, proto můžu použít Optional chaining a to error nehodí ikdyž to bude undefined
+
+const person = { };
+
+const dude = person.name;
+console.log(foo); // Uncaught TypeError: Cannot read property 'bar' of undefined
+
+const dude = person?.name; // undefined
+
+// Nullish coalescing - Nullish coalescing is a relativly new operator that was introduced in ES2020. It is similar to the logical OR operator ||, but it only returns the right-hand side if the left-hand side is null or undefined.
+
+const foo = null ?? 'bar';
+console.log(foo); // 'bar'
+
+const foo = 0 ?? 'bar';
+console.log(foo); // 0
